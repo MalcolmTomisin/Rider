19af5160c6fbc68057fd77b193821b78a4419c81
Tomisin <malcolmtomisin@gmail.com>
2020-11-23 13:52:52 +0100

    new screen

---
+++ app/screens/Dashboard/order/completed.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {View, StyleSheet, ScrollView} from 'react-native';
+import { Task } from '../../../components/Card'
+import {useSelector} from 'react-redux';
+
+
+const CompletedOrder = () => {
+
+  return (
+    <View style={classes.root}>
+      <ScrollView>
+      {/* {
+        !data && !message.accept ? null : renderTasks(data?.orders)
+      } */}
+      </ScrollView>
+    </View>
+  );
+}
+
+export default CompletedOrder;
+
+const classes = StyleSheet.create({
+  root: {
+    flex: 1,
+    marginHorizontal: 20,
+    paddingVertical: 15,
+    justifyContent: 'space-evenly'
+  },
+});
