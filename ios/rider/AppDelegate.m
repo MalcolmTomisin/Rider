b389b709c31f2d5fd8667d9a6a0107093414c371
Tomisin <malcolmtomisin@gmail.com>
2021-01-21 12:00:06 +0100

    obj file

--- ios/rider/AppDelegate.m
+++ ios/rider/AppDelegate.m
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import <Firebase.h>
 #import <GoogleMaps/GoogleMaps.h>
 
 #import <React/RCTBridge.h>
@@ -28,6 +29,9 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
   [GMSServices provideAPIKey:@"AIzaSyCiOd5vESI31DmPFd6e7QVRVMTX43sm_Ic"];
 
 #ifdef FB_SONARKIT_ENABLED
