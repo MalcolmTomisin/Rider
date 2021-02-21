76d661562769a7784c2cd657e822f5d2f1ede187
Tomisin <malcolmtomisin@gmail.com>
2021-02-21 20:15:30 +0100

    change support

--- app/screens/Dashboard/profile/support.js
+++ app/screens/Dashboard/profile/support.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import LiveChat from 'react-native-livechat';
+import React, {useState} from 'react';
+import {SafeAreaView, Modal} from 'react-native';
+import WebView from 'react-native-webview';
 import constants from '../../../utils/constants';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-const Support = () => (
-  <LiveChat
-    license={constants.LICENSE_ID}
-    redirectUri={constants.REDIRECT_URL}
-    clientId={constants.CLIENT_ID}
-  />
-);
+const Support = ({navigation: {navigate, goBack}}) => {
+  const [visible, setVisible] = useState(true);
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <Modal visible={visible}>
+        <Icon style={{margin: 20}} size={30} name="cancel" onPress={() => {
+          setVisible(false)
+          goBack();
+          }} />
+        <WebView
+          source={{
+            uri: `https://secure.livechatinc.com/licence/${constants.LICENSE_ID}/v2/open_chat.cgi`,
+          }}
+        />
+      </Modal>
+    </SafeAreaView>
+  );
+}
 
 export default Support;
